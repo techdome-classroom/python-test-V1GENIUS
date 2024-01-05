b3b7feecc1e7bae84ed303c165930ff2aab1a7f1
Vivek rathore <93070457+V1GENIUS@users.noreply.github.com>
2024-01-05 13:38:17 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,11 +1,24 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
-    
+def Solution(s):
+    stack = []
+    for ch in s:
+        if ch in ('(', '{', '['):
+            stack.append(ch)
+        else:
+            if stack and ((stack[-1] == '(' and ch == ')') or
+                        (stack[-1] == '{' and ch == '}') or
+                        (stack[-1] == '[' and ch == ']')):
+                stack.pop()
+            else:
+                return False
+    return not stack
+
+expr = "(]"
+
+# Function call
+if Solution(expr):
+    print("true")
+else:
+    print("false")
 
 
 
